c07cd0baa981cba619dcaf79260848e25bea1258
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-05-05 13:35:16 -0400

    add price display component to main app component; add styles to ensure children components use the same width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ImageContainerComponent } from './image-container/image-container.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ImageContainerComponent],
+  imports: [ImageContainerComponent, PriceDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
