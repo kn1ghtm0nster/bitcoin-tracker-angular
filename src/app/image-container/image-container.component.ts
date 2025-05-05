3fc76606a204f4c41b132e7a226f5a40b68ee1d6
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-05-05 00:04:52 -0400

    add image container component

---
+++ src/app/image-container/image-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-container',
+  imports: [],
+  templateUrl: './image-container.component.html',
+  styleUrl: './image-container.component.css'
+})
+export class ImageContainerComponent {
+
+}
