8a9588ed4f658608153a7980d7b74e81a600d4c3
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-05-05 13:34:39 -0400

    update starting html conent and styles

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-price-display',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './price-display.component.html',
-  styleUrl: './price-display.component.css'
+  styleUrl: './price-display.component.css',
 })
-export class PriceDisplayComponent {
-
-}
+export class PriceDisplayComponent {}
